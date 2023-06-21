1d06bb536cfb152f95cc7aeabeba38e68118ee2f
Mia564 <115929217+Mia564@users.noreply.github.com>
2023-06-21 09:57:43 +0900

    SceneManager追加
    とりあえず画面切り替えのみ
    ローディングは怪しい点があるので治ったら追加予定

--- Source/SceneGame.h
+++ Source/SceneGame.h
@@ -3,25 +3,26 @@
 #include "Stage.h"
 #include "Player.h"
 #include "CameraController.h"
+#include "Scene.h"
 
 // ゲームシーン
-class SceneGame
+class SceneGame : public Scene
 {
 public:
 	SceneGame() {}
-	~SceneGame() {}
+	~SceneGame() override {}
 
 	// 初期化
-	void Initialize();
+	void Initialize() override;
 
 	// 終了化
-	void Finalize();
+	void Finalize() override;
 
 	// 更新処理
-	void Update(float elapsedTime);
+	void Update(float elapsedTime) override;
 
 	// 描画処理
-	void Render();
+	void Render() override;
 
 private:
 	Stage* stage = nullptr;
