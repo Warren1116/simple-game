d09c3f9f930958c70390dd3ced5dadfa90ff59b0
Mia564 <115929217+Mia564@users.noreply.github.com>
2023-07-11 16:29:01 +0900

    自機の調整

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -409,7 +409,7 @@ void Player::DrawOver(ID3D11DeviceContext* dc)
 void Player::UpdateVerticalVelocity(float elapsedFrame) {
     // 燃料を使っていなければ縦に重力を加える
     if (!fuelUse) {
-        if (hasSpeed) {
+        if (hasSpeed && fuel > 0) {
             float speed = gravity * elapsedFrame;
 
             if (velocity.z > MinVelocityZ) velocity.y += speed * GravityYAdjustmentNormal;
@@ -452,8 +452,8 @@ void Player::UpdateHorizontalVelocity(float elapsedFrame) {
         //横方向の速力が摩擦力以下になったので速力を無効化
         else
         {
-            velocity.x = 0.0f;
-            velocity.y = 0.0f;
+            velocity.x = 0;
+            velocity.y = 0;
             velocity.z = MinVelocityZ;
         }
     }
@@ -492,7 +492,7 @@ void Player::GravityAdjust(float elapsedFrame) {
 
     float rotate = sinf(angle.x - DirectX::XMConvertToRadians(90)); // ロジックの中で90度引いてるのはよくない
     float speed = gravity * elapsedFrame;
-    float rot = 1.0f - rotate * 0.8f/* - rotate*/; // ここが問題点
+    float rot = 1.0f - rotate * 0.5f/* - rotate*/; // ここが問題点
     // 下向き
     //if (rotate > 0 && onAddSpeed) velocity.z -= speed * rot * GravityZAdjustmentAdd;
     // 上向き
@@ -501,14 +501,13 @@ void Player::GravityAdjust(float elapsedFrame) {
     // 下向き
     if (rotate > DirectX::XMConvertToRadians(0)) {
         onSpeed = true;
-        acceleration += -speed * rot * 0.5f;
-        if (acceleration > 40) acceleration = 40;
+        acceleration += -speed * rot * 0.2f;
     }
     //else if (rotate == DirectX::XMConvertToRadians(0)) onSpeed = false;
     // 上向き
     else/* if (rotate < 0 && acceleration > MinAcceleSpeed)*/ {
         onSpeed = false;
-        acceleration -= -speed * rot * GravityZAdjustmentSub;
+        acceleration -= -speed * rot * 0.2f;
         if (acceleration < MinAcceleSpeed) acceleration = MinAcceleSpeed;
     }
 }
